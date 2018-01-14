7dc49b6bb54465e81d5331ac30f8c119b0ab333c
jaredramirez <jaredramirez@me.com>
2018-01-13 18:12:21 -0600

    Add simple example

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const resolvePath = relativePath => path.resolve(__dirname, relativePath);
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+
+  entry: resolvePath('src/simple/reason/Main.re'),
+
+  output: {
+    path: resolvePath('lib'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: require.resolve('babel-loader')},
+      {
+        test: /\.(re|rei)$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: require.resolve('bs-loader'),
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: require.resolve('elm-webpack-loader'),
+          options: {
+            cwd: resolvePath('.'),
+            forceWatch: true,
+            debug: true,
+          },
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.re', '.rei', '.elm'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: resolvePath('public/index.html'),
+    }),
+  ],
+};
